01de805d4045b9c44bd71ad6c0ba5acb0393c522
Luis <luisgmgomes@gmail.com>
2025-04-14 22:06:34 -0300

    create: home

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
